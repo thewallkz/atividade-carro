9ae9594d7bdd17f519a9e611e6826435adc19cac
Bruno Anjos <enoixbro@gmail.com>
2025-10-22 17:37:12 -0300

    Update Program.cs

--- carro/Program.cs
+++ carro/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿using carro;
+
+Carro meuCarro = new Carro();
+meuCarro.cor = "Azul";
+meuCarro.modelo = "Sedan";
+meuCarro.velocidadeMaxima = 160;
+
+meuCarro.liga();
+meuCarro.acelera(50);
+meuCarro.passarMarcha();
